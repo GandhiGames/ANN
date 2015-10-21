19d5960ed05b23c55cf5fe1ba47939eadd29b562
Robert Wells <robert@Roberts-MacBook-Pro.local>
2015-10-21 11:29:08 +0100

    Initial Commit

---
+++ NeuralNet/Scripts/ANNTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Simulation;
+
+[RequireComponent (typeof(NeuralNetController), typeof(Utilities))]
+public class ANNTest : MonoBehaviour
+{
+	private NeuralNetController _netController;
+	
+	void Start ()
+	{
+		_netController = GetComponent<NeuralNetController> ();
+	}
+
+	void Update ()
+	{
+		Utilities.instance.PrettyPrint (_netController.UpdateNeuralNet (Utilities.instance.RandomInputBuilder (_netController)));
+	}
+}
